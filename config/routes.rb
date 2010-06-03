e2787a1b359adcdbbd2cb7ff4e66bacce67eb0ec
sch1zo <andreas.eger.reg@googlemail.com>
2010-06-03 02:45:43 +0200

    inital heroku bamboo push
    Signed-off-by: sch1zo <andreas.eger.reg@googlemail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Media::Application.routes.draw do |map|
   resources :languages
 
-
   resources :requests
 
   resources :genres
@@ -62,7 +61,7 @@ Media::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "entries#index"
 
   # See how all your routes lay out with "rake routes"
 
